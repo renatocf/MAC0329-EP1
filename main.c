509bcfd915b5c997f1f19934cfddef97bbdbbcf6
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-03-13 22:26:40 -0300

    Separating file in library header (analyzer.h), library source (analyzer.c) and main (main.c). Makefile now compiles with all these files.

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "analyzer.h"
+
+int main(int argc, char **argv)
+{
+    char **teste; int i; /* char *ch; char a = 'a'; */
+    teste = (char **) malloc(MAX_HEIGHT * sizeof(char *));
+    for(i = 0; i < MAX_HEIGHT; i++)
+    {
+        teste[i] = (char *) malloc(MAX_WIDTH * sizeof(char));
+    }
+    
+    teste[0] = "INN20";
+    teste[1] = "ADD20";
+    teste[2] = "+2020";
+    teste[3] = "\n";
+    teste[4] = "-2020";
+    
+    analyse_convert(teste);
+    
+    printf("After conversion:\n");
+    for(i = 0; i < MAX_HEIGHT-80; i++)
+        printf("%2.d %s\n", i, teste[i]);
+    
+    return 0;
+}
